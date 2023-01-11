d6159262a4e7eb63ad362f192797c7e809cdff30
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-01-11 10:12:39 +0000

    Update zfs-install-script.sh

--- zfs-install-script.sh
+++ zfs-install-script.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
-DRIVE=/dev/nvme1p1
+echo"_____  ___                ___                            __    ____  _____ "
+echo"|    |/ _|____    _______/  |_  ____   ____             |  | _/_   \   _  \ "
+echo"|      < \__  \  /  ___/\   __\/ __ \ /    \    ______  |  |/ /|   /  /_\  \ "
+echo"|    |  \ / __ \_\___ \  |  | \  ___/|   |  \  /_____/  |    < |   \  \_/   \ "
+echo"|____|__ (____  /____  > |__|  \___  >___|  /           |__|_ \|___|\_____  / "
+echo"        \/    \/     \/            \/     \/                 \/           \/ "
+echo"Welcome to the script runner!"
+echo""
+echo"This will install a single node k3s cluster with zfs-csi driver, zfs storageclass and k10 annotated volumesnapshotclass"
+echo"It will then install k10 via HELM and automatically expose the k10 dashboard on the cluster load balancer"
+
+read -p "Enter drive path of extra volume (ie /dev/sdb). If you do not know this exit this script by cmd-x and run "fdisk -l" to find the drive path: " DRIVE
 
 apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable local-storage" sh -s -
@@ -19,3 +30,7 @@ helm install k10 kasten/k10 --namespace kasten-io
 sleep 60
 pod=$(kubectl get po -n kasten-io |grep gateway | awk '{print $1}' )
 kubectl expose po $pod -n kasten-io --type=LoadBalancer --port=8000 --name=k10-dashboard
+ip=$(curl -s ifconfig.io)
+port=$(kubectl get svc -n kasten-io |grep k10-dashboard | cut -d':' -f2- | cut -f1 -d'/' )
+echo ""
+echo "K10 dashboard can be accessed on" $ip":"$port
