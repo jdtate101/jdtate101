a2b1f9e5eb69ee985435f3d779824a30631de880
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-07-17 15:01:31 +0100

    Update kubevirt.sh

--- kubevirt.sh
+++ kubevirt.sh
@@ -9,7 +9,7 @@ echo ""
 echo "Installing Krew and virt plugin..."
 echo ""
 sleep 1
-(   set -x; cd "$(mktemp -d)" &&   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.4.4/krew-linux_amd64.tar.gz" &&   tar zxvf krew-linux_amd64.tar.gz &&   KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&   "$KREW" install krew; )
+(  set -x; cd "$(mktemp -d)" &&  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&  KREW="krew-${OS}_${ARCH}" &&  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&  tar zxvf "${KREW}.tar.gz" &&  ./"${KREW}" install krew)
 export PATH="${PATH}:${HOME}/.krew/bin"
 echo "export PATH="${PATH}:${HOME}/.krew/bin"" >> /root/.bashrc
 kubectl krew install virt
@@ -30,9 +30,6 @@ echo ""
 sleep 1
 echo "Install Virtctl..."
 echo ""
-export VIRTCTLVERSION=v0.41.0
-wget https://github.com/kubevirt/kubevirt/releases/download/${VIRTCTLVERSION}/virtctl-${VIRTCTLVERSION}-linux-amd64
-mv  ./virtctl-v0.41.0-linux-amd64 /usr/local/bin/virtctl
-chmod +x /usr/local/bin/virtctl
+(  set -x; cd "$(mktemp -d)" &&  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" && VIRTCTL="virtctl-v${VERSION}-${OS}-${ARCH}" && wget "https://github.com/kubevirt/kubevirt/releases/download/v${VERSION}/${VIRTCTL}" && mv virtctl* /usr/local/bin/virtctl && chmod +x /usr/local/bin/virtctl )
 echo ""
 exit 
