e80bfa8fe328ad4bea89cb91abbfaa0346f3530b
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-07-31 12:09:00 +0100

    Update import-string-dump.sh

--- import-string-dump.sh
+++ import-string-dump.sh
@@ -1,5 +1,6 @@
 policies=$(kubectl get policies -n kasten-io -A | awk '{print $2}' | tail -n +2 | grep -vE 'k10-disaster-recovery-policy|k10-system-reports-policy')
 for policies in $policies; do
   string=$(kubectl describe policy $policies -n kasten-io |grep "Receive String"| cut -c 7- | sed 's/Receive/Import/g')
-  echo "Policy:" $policies $'\n'$string$'\n'
+  echo "Policy:" $policies $'\n '$string''
+  echo ""
 done
