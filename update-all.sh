aa678ac95ddab5cfa0e05ed498c4de1d40158820
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-08-04 07:21:10 +0100

    Update update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -1,4 +1,4 @@
-SSHPAS=Sh1njuku02
+SSHPAS=*********
 neofetch
 sudo aptitude update --no-gui && sudo aptitude safe-upgrade -y --no-gui && sudo aptitude autoclean -y --no-gui
 echo "*********************"
