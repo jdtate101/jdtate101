d9468ee724101a2dcfb29679ba619a4d07fc464d
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-08-04 15:02:40 +0100

    Update rapid-lab.sh

--- rapid-lab.sh
+++ rapid-lab.sh
@@ -54,7 +54,7 @@ sleep 5
 echo -e "$G Installing k3s single node cluster"
 echo -e "$W "
 sleep 5
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.26" INSTALL_K3S_EXEC="--disable local-storage" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable local-storage" sh -s -
 sleep 5
 echo ""
 echo -e "$G Installing ZFS and configuring pool"
