f269d10eb91ef980d38139021a283bfd1246e66c
James Tate <60175841+jdtate101@users.noreply.github.com>
2023-10-30 10:30:02 +0000

    Update reg-push.sh

--- reg-push.sh
+++ reg-push.sh
@@ -3,7 +3,7 @@
 # Define the target registry
 echo "Enter version you want to pull (format 5.0.5): "
 read VERSION < /dev/tty
-TARGET_REGISTRY="reg.lab.home"
+TARGET_REGISTRY="registry.example.com"
 IMAGES=$(docker images | grep "$VERSION" | awk '{print $1":"$2}')
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock gcr.io/kasten-images/k10offline:$VERSION pull images
 # Loop through the list of images and push them to the target registry
